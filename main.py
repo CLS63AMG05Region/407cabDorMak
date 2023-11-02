3f3a4372b05a0cf2367086030f6c9598e070a47a
CLS63AMG05Region <148055798+CLS63AMG05Region@users.noreply.github.com>
2023-11-02 16:30:47 +0300

    second

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 a = int(input("Type number one: "))
 if a == 1:
     print("Correct")
+    b = int(input("Type number two: "))
+    if b == 2:
+        print("Correct")
+    else:
+        print("You are under arrest")
 else:
-    print("U are under arrest")
+    print("You are under arrest")
